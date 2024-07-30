f984c7c494de7d69475ba31cecafa63ee09c396e
unknown <owlstudio.dsign@gmail.com>
2024-07-30 21:10:47 +0700

    update navbar hamburger menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { TiThLarge } from "react-icons/ti";
 
 const Navbar = () => {
   return (
@@ -11,24 +12,29 @@ const Navbar = () => {
               <span>SimplySpaces</span>
             </Link>
           </div>
-          <nav className="list">
-            <ul role="list">
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/posts">Listing</Link>
-              </li>
-              <li>
-                <Link to="/agents">Agents</Link>
-              </li>
-            </ul>
-          </nav>
-          <div className="actions">
-            <Link to="/auth/login">Sign in</Link>
-            <Link className="button button--primary" to="/auth/register">
-              Join
-            </Link>
+          <div className="menu">
+            <button className="button button--link toggler" type="button">
+              <TiThLarge size={24} color="red" />
+            </button>
+            <nav className="list">
+              <ul role="list">
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/posts">Listing</Link>
+                </li>
+                <li>
+                  <Link to="/agents">Agents</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="actions">
+              <Link to="/auth/login">Sign in</Link>
+              <Link className="button button--primary" to="/auth/register">
+                Join
+              </Link>
+            </div>
           </div>
         </div>
       </div>
